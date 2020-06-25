eb744a9325e206d14024fb9e2b33383435f80e98
M4YZZ <66739243+M4YZZ@users.noreply.github.com>
2020-06-25 12:04:14 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,33 +3,67 @@
  provider "aws" {
    access_key = ""
    secret_key = ""
-   region = "var.region"
+   region = var.region
  }
 
 # Retrieve the AZ where we want to create network resources
-data "aws_availability_zones" "available" {}
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+data "aws_vpc" "selected" {
+  id = var.aws_vpc
+}
 
 # VPC Resource
-resource "aws_vpc" "main" {
-  cidr_block = "10.11.0.0/16"
-  region = "var.region"
-  enable_dns_support = true
-  enable_dns_hostnames = true
-  tags {
-    Name = "Test-VPC"
-  }
-  tags {
-    Environment = "Test"
-  }
-}
+#resource "aws_vpc" "main" {
+ # cidr_block = "10.11.0.0/16"
+  #enable_dns_support = true
+  #enable_dns_hostnames = true
+  #tags = {
+   # Name = "Test-VPC"
+    #Environment = "Test"
+   #}
+#}
 
 # AWS subnet resource
 resource "aws_subnet" "test" {
- vpc_id = "${aws_vpc.main.id}"
+ vpc_id = data.aws_vpc.selected.id
  cidr_block = "10.11.1.0/24"
- availability_zone = "${data.aws_availability_zones.available.names[0]}"
+
+ #data.aws_availability_zones.available.names[0]= will create subnet in 2a [1]-2b [2]-2c
+ availability_zone = data.aws_availability_zones.available.names[0]
  map_public_ip_on_launch = "false"
- tags {
+ tags = {
    Name = "Test_subnet1"
  }
 }
+
+# AWS instance
+#resource "aws_instance" "web" {
+  #ami = "ami-3b261642"
+  #instance_type = "t1.micro"
+  #tags = {
+    #Name = "myInstance"
+  #}
+#}
+
+  # AWS S3 bucket
+  #resource "aws_s3_bucket" "tfstatebucket" {
+  #bucket = "rl.tfstate"
+  #acl    = "private"
+  #versioning {
+    #enabled = false
+  #}
+
+  # We also want to make sure our bucket enables server-side encryption
+  #server_side_encryption_configuration {
+    #rule {
+      #apply_server_side_encryption_by_default {
+       # sse_algorithm = "AES256"
+      #}
+    #}
+  #}
+  #tags = {
+   # Name = "s3bucket"
+  #}
+#}
